8ddd60768b8c2941b3c8b2e041f847f858db52da
Ahmeed-Sayed <ahmeedsayed01@gmail.com>
2024-10-27 21:40:12 +0300

    fix: added max/min height/width to the app

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import { HeaderProps } from "./utils/interfaces";
 const Layout: React.FC<HeaderProps> = ({ setMode }) => {
   return (
-    <Box display={"flex"} gap={3}>
+    <Box display={"flex"} gap={3} minWidth="1280px" minHeight="768px">
       <SidebarComponent />
       <Box width={"100%"}>
         <Header setMode={setMode} />
